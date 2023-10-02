b182ef34c44357c3f79843511cda59e0894a13e2
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2023-10-01 21:57:18 -0400

    add resume link

--- src/components/Main/About/index.js
+++ src/components/Main/About/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import resume from "../../../assets/documents/Ian_resume.pdf";
 
 function About () {
     return (
@@ -8,6 +9,7 @@ function About () {
             <p>I am passionate about making life more sustainable without impacts to everyday life. I have engaged in a study abroad program based in Paris, France for sustainability and I have been working as a Co-Op for an Energy Company specializing in Greenfield projects to bring more wind, solar and battery storage online. In addition to a sustainability passion, I am hungry to create and provide the most economical and reliable solutions for products and services.</p>
             <p>I consider my tenure servicing and maintaining a fleet of over 50 school buses to be one of my biggest professional achievements, due to the satisfaction of providing safe travel for tens of thousands of children every day. I believe this achievement of hard work trumps other achievements of innovation, due to the service provided for future generations. A personal accomplishment I am prouder of than any other is my hand in creating and running the Formula SAE Electric Vehicle team at UNCC. This team provided a safe haven for future engineers and other majors alike. The team was founded to not single any one person out, and allow those individuals to grow while learning career skills, such as corporate outreach, marketing, financial planning, project planning, project engineering, and communications.</p>
             <p>I believe in thoroughness and thoughtfulness in the workplace and in my personal life. I am an avid cyclist, father, husband, innovator, and friend. I look towards the future in terms of sustainability and creating the best environment, economy, and culture for future generations. I actively wants to help fill the void in sustainable manufacturing, energy and construction.</p>
+            <p><a href={resume} download="Ian-Sullivan_Resume">Click HERE to download my resume.</a></p>
         </div>
     )
 }
